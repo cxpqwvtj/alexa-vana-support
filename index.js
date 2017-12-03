b92227996a0d271fcd9b048ff556e543bdb54f0f
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-03 13:35:52 +0900

    indentを2に修正

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var Alexa = require('alexa-sdk');
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
  
 var handlers = {
-    'Unhandled': function () {
-        this.emit(':tell', 'よい旅を');
-    }
+  'Unhandled': function () {
+    this.emit(':tell', 'よい旅を');
+  }
 };
