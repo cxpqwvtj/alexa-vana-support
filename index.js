3921a761577a7cc386e678d2708ce607c59a6180
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-03 18:14:30 +0900

    リクエストをログ出力するように修正

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Alexa = require('alexa-sdk');
 exports.handler = function(event, context, callback) {
+  console.log(event);
   var alexa = Alexa.handler(event, context);
   alexa.appId = process.env.APP_ID;
   alexa.registerHandlers(handlers);
