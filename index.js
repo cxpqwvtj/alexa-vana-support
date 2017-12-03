741ea83aefe86e34ddd8479f0ae8ece040167c66
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-03 13:42:12 +0900

    appIdを設定

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Alexa = require('alexa-sdk');
 exports.handler = function(event, context, callback) {
   var alexa = Alexa.handler(event, context);
+  alexa.appId = process.env.APP_ID;
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
