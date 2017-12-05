4dc5c145194f26caeae537f73205c2a1431a64c4
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-05 12:40:10 +0900

    現在の期間限定目標と次の期間限定目標を答えてくれる様に修正

--- handlers/LimitedTimeChallenges.js
+++ handlers/LimitedTimeChallenges.js
@@ -1,24 +1,46 @@
-module.exports = function() {
-  const limitedTimeChallenges = [
-    ['魔法ダメージで倒す', 'プラントイド類を倒す', 'アモルフ類を倒す', 'ヴァーミン類を倒す', 'アルカナ類を倒す', '経験値を得る'], // 日曜
-    ['バード類を倒す', '', '', '', '', ''],
-  ]
+const moment = require('moment')
 
-  const hour = moment().utcOffset('+09:00').diff(moment().utcOffset('+09:00').startOf('day'), 'hour')
-  console.log(hour)
-  let message = ''
+const limitedTimeChallenges = [
+  ['魔法ダメージで倒す', 'プラントイド類を倒す', 'アモルフ類を倒す', 'ヴァーミン類を倒す', 'アルカナ類を倒す', '経験値をえる'], // 日曜
+  ['バード類を倒す', 'リザード類を倒す', 'アンデッド類を倒す', '戦利品(印章)', 'Treasure Casketを開ける', 'アクアン類を倒す'], // 月曜
+  ['アモルフ類を倒す', 'ヴァーミン類を倒す', 'アルカナ類を倒す', '経験値をえる', '物理ダメージで倒す', 'ビースト類を倒す'], // 火曜
+  ['アンデッド類を倒す', '戦利品(印章)', 'Treasure Casketを開ける', 'アクアン類を倒す', '魔法ダメージで倒す', 'プラントイド類を倒す'], // 水曜
+  ['アルカナ類を倒す', '経験値をえる', '物理ダメージで倒す', 'ビースト類を倒す', 'バード類を倒す', 'リザード類を倒す'], // 木曜
+  ['Treasure Casketを開ける', 'アクアン類を倒す', '魔法ダメージで倒す', 'プラントイド類を倒す', 'アモルフ類を倒す', 'ヴァーミン類を倒す'], // 金曜
+  ['物理ダメージで倒す', 'ビースト類を倒す', 'バード類を倒す', 'リザード類を倒す', 'アンデッド類を倒す', '戦利品(印章)'], // 土曜
+]
+
+module.exports = function() {
+  const nowChallenge = challengeName(moment().utcOffset('+09:00'))
+  const nextChallenge = challengeName(moment().utcOffset('+09:00').add(4, 'hour'))
+  this.emit(':tell', `現在の期間限定目標は、${nowChallenge.name} です。次は、${nextChallenge.startHour}時から${nextChallenge.name} です。`)
+}
+
+const challengeName = (time) => {
+  const hour = time.diff(moment().utcOffset('+09:00').startOf('day'), 'hour')
+  let timeRangeIndex
+  let startHour
   if (0 <= hour && hour < 4) {
-    message = limitedTimeChallenges[moment().weekday()][0]
+    timeRangeIndex = 0
+    startHour = 0
   } else if (4 <= hour && hour < 8) {
-    message = limitedTimeChallenges[moment().weekday()][1]
+    timeRangeIndex = 1
+    startHour = 4
   } else if (8 <= hour && hour < 12) {
-    message = limitedTimeChallenges[moment().weekday()][2]
+    timeRangeIndex = 2
+    startHour = 8
   } else if (12 <= hour && hour < 16) {
-    message = limitedTimeChallenges[moment().weekday()][3]
+    timeRangeIndex = 3
+    startHour = 12
   } else if (16 <= hour && hour < 20) {
-    message = limitedTimeChallenges[moment().weekday()][4]
+    timeRangeIndex = 4
+    startHour = 16
   } else if (20 <= hour && hour < 24) {
-    message = limitedTimeChallenges[moment().weekday()][5]
+    timeRangeIndex = 5
+    startHour = 20
   }
-  this.emit(':tell', `${message} です`)
+  return {
+    name: limitedTimeChallenges[time.weekday()][timeRangeIndex],
+    startHour
+  }
 }
