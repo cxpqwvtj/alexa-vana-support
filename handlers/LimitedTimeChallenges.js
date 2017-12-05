147732911731e69136ab851600246c67e512f181
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-05 12:05:06 +0900

    期間限定目標の処理を別ファイルに移動

---
+++ handlers/LimitedTimeChallenges.js
@@ -0,0 +1,24 @@
+module.exports = function() {
+  const limitedTimeChallenges = [
+    ['魔法ダメージで倒す', 'プラントイド類を倒す', 'アモルフ類を倒す', 'ヴァーミン類を倒す', 'アルカナ類を倒す', '経験値を得る'], // 日曜
+    ['バード類を倒す', '', '', '', '', ''],
+  ]
+
+  const hour = moment().utcOffset('+09:00').diff(moment().utcOffset('+09:00').startOf('day'), 'hour')
+  console.log(hour)
+  let message = ''
+  if (0 <= hour && hour < 4) {
+    message = limitedTimeChallenges[moment().weekday()][0]
+  } else if (4 <= hour && hour < 8) {
+    message = limitedTimeChallenges[moment().weekday()][1]
+  } else if (8 <= hour && hour < 12) {
+    message = limitedTimeChallenges[moment().weekday()][2]
+  } else if (12 <= hour && hour < 16) {
+    message = limitedTimeChallenges[moment().weekday()][3]
+  } else if (16 <= hour && hour < 20) {
+    message = limitedTimeChallenges[moment().weekday()][4]
+  } else if (20 <= hour && hour < 24) {
+    message = limitedTimeChallenges[moment().weekday()][5]
+  }
+  this.emit(':tell', `${message} です`)
+}
