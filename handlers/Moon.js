5ac77b3d40be90453191586fff459d464879f563
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-07 06:44:25 +0900

    釣りに影響を与える旨のコメントを追加

--- handlers/Moon.js
+++ handlers/Moon.js
@@ -2,5 +2,5 @@ const utils = require('../utils')
 
 module.exports = function() {
   const {vanaMoonJp} = utils.vanaTime()
-  this.emit(':tell', `現在の月齢は${vanaMoonJp} です。`)
+  this.emit(':tell', `現在の月齢は${vanaMoonJp} です。満月と新月は、釣りの成果が向上します。上弦の月と下弦の月は、釣りの成果が低下します。`)
 }
