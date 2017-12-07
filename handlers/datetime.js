090c74baa07d0255f6f1687d18463912bc6a0eeb
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-08 06:21:46 +0900

    lint対応

--- handlers/datetime.js
+++ handlers/datetime.js
@@ -1,7 +1,7 @@
 const utils = require('../utils')
 
 module.exports = function() {
-  const {vanaYear, vanaMonth, vanaDay, vanaHour, vanaMinute, vanaSec, vanaWeek, vanaMoon, vanaWeekJp, vanaMoonJp} = utils.vanaTime()
+  const {vanaYear, vanaMonth, vanaDay, vanaHour, vanaMinute, vanaSec, vanaWeekJp, vanaMoonJp} = utils.vanaTime()
 
   const vanaTime = `${vanaYear}年${vanaMonth}月${vanaDay}日 ${vanaHour}時${vanaMinute}分${vanaSec}秒`
   console.log(vanaTime, vanaWeekJp, vanaMoonJp)
