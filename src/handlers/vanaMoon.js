18a2e16eb7be60a8da26296ce1696e6a62b0bdd0
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-09 11:41:54 +0900

    釣りの成果が上下するときだけコメントする様に修正

--- src/handlers/vanaMoon.js
+++ src/handlers/vanaMoon.js
@@ -1,6 +1,18 @@
 const utils = require('../utils')
 
 module.exports = function() {
-  const {vanaMoonJp} = utils.vanaTime()
-  this.emit(':tell', `現在の月齢は${vanaMoonJp} です。満月と新月は、釣りの成果が向上します。上弦の月と下弦の月は、釣りの成果が低下します。`)
+  const {vanaMoon, vanaMoonJp} = utils.vanaTime()
+  // 満月と新月は、釣りの成果が向上します。上弦の月と下弦の月は、釣りの成果が低下します。
+  const effect = (() => {
+    if (vanaMoon === 2 || vanaMoon === 8) {
+      // 満月か新月の時
+      return '釣りの成果が向上します。'
+    } else if (vanaMoon === 5 || vanaMoon === 11) {
+      // 上弦の月か下弦の月の時
+      return '釣りの成果が低下します。'
+    } else {
+      return ''
+    }
+  })()
+  this.emit(':tell', `現在の月齢は${vanaMoonJp} です。${effect}`)
 }
