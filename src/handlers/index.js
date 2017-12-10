e7e0b8aeec1317b3a88d01789789b173201c4577
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-10 19:29:33 +0900

    起動時、ヘルプ、例外時のメッセージ調整

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -6,16 +6,16 @@ const mummersCoalitionBoost = require('./mummersCoalitionBoost')
 
 module.exports = {
   'LaunchRequest': function () {
-    this.emit('AMAZON.HelpIntent')
+    this.emit(':ask', 'ようこそ。ヴァナサポへ', message.LINE_CLOSE)
   },
   'AMAZON.HelpIntent': function () {
-    this.emit(':ask', 'ようこそ。ヴァナサポへ', message.LINE_CLOSE)
+    this.emit(':ask', 'ようこそ。ヴァナサポへ。時間を知りたい場合は、時間を教えて。と聞いてください。', message.LINE_CLOSE)
   },
   'tora': function () {
     this.emit(':ask', 'とら', message.LINE_CLOSE)
   },
   'Unhandled': function () {
-    this.emit(':tell', 'INTよん？で理解できません。勉強してきます。')
+    this.emit(':tell', 'いんと よん？ で理解できません。勉強してきます。')
   },
   Datetime,
   Moon,
