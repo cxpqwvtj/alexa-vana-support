e42f3b5761428acda8723e12b3aa7a5cd54a4e6a
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-10 19:30:50 +0900

    ヘルプメッセージ変更

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -9,7 +9,7 @@ module.exports = {
     this.emit(':ask', 'ようこそ。ヴァナサポへ', message.LINE_CLOSE)
   },
   'AMAZON.HelpIntent': function () {
-    this.emit(':ask', 'ようこそ。ヴァナサポへ。時間を知りたい場合は、時間を教えて。と聞いてください。', message.LINE_CLOSE)
+    this.emit(':ask', '時間を知りたい場合は、時間を教えて。と聞いてください。', message.LINE_CLOSE)
   },
   'tora': function () {
     this.emit(':ask', 'とら', message.LINE_CLOSE)
