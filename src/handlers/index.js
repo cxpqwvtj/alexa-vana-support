b35912ebeabb8f38d8d52f18324b3b1a4ace7ea2
cxpqwvtj <cxpqwvtj@gmail.com>
2017-12-26 10:52:25 +0900

    コードフォーマット調整

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -8,16 +8,16 @@ const mummersCoalitionBoost = require('./mummersCoalitionBoost')
 const selectCoalition = require('./selectCoalition')
 
 module.exports = {
-  'LaunchRequest': function () {
+  'LaunchRequest': function() {
     this.emit(':ask', 'ようこそ。ヴァナサポへ。使い方を知りたい場合は、使い方を教えてと話しかけてください。', message.LINE_CLOSE)
   },
-  'AMAZON.HelpIntent': function () {
+  'AMAZON.HelpIntent': function() {
     this.emit(':ask', '時間を知りたい場合は、時間を教えて。と話しかけてください。', message.LINE_CLOSE)
   },
-  'AMAZON.CancelIntent': function () {
+  'AMAZON.CancelIntent': function() {
     this.emit(':tell', 'キャンセルしました。よい旅を')
   },
-  'AMAZON.StopIntent': function () {
+  'AMAZON.StopIntent': function() {
     this.emit(':tell', 'ストップしました。よい旅を')
   },
   'tora': function () {
