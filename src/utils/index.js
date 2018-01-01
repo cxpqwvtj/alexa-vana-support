d7eac192e9f49345b9c473cf809385cc7e1142a4
cxpqwvtj <cxpqwvtj@gmail.com>
2018-01-02 08:55:25 +0900

    次の光曜日を返す様に変更

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,32 +6,47 @@ module.exports = {
     const weekDays = ['火', '土', '水', '風', '氷', '雷', '光', '闇']
     const moonList = ['二十日月', '二十六夜', '新月', '三日月', '七日月', '上弦の月', '十日月', '十三夜', '満月', '十六夜', '居待月', '下弦の月']
 
-    const vanaTotalPassSec = (moment().unix() + 92514960) * 25
-    const vanaWeek = Math.floor( vanaTotalPassSec / (24 * 60 * 60) ) % 8
+    // ヴァナ時間計算規準日
+    const basisDate = moment('2002/06/24 00:00:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').toDate()
+
+    // 曜日計算規準日(火曜日始点)
+    const basisWeekDay = moment('2002/06/26 00:00:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').toDate()
+
+    const msGameDay = (24 * 60 * 60 * 1000 / 25) // milliseconds in a game day
+    const msRealDay = (24 * 60 * 60 * 1000) // milliseconds in a real day
+
+    const now = moment().toDate()
+    const vanaDate = ((898 * 360 + 30) * msRealDay) + (now.getTime() - basisDate.getTime()) * 25
+
+    // 次の光曜日計算
+    const basisLightDay = basisWeekDay.getTime() - (msGameDay * 2)
+    const weekCount = Math.floor((now.getTime() - basisLightDay) / (msGameDay * weekDays.length)) + 1
+    const nextLightDay = basisLightDay + ((msGameDay * weekDays.length) * weekCount)
+
+    const vYear = Math.floor(vanaDate / (360 * msRealDay))
+    const vMon  = Math.floor((vanaDate % (360 * msRealDay)) / (30 * msRealDay)) + 1
+    const vDate = Math.floor((vanaDate % (30 * msRealDay)) / (msRealDay)) + 1
+    const vHour = Math.floor((vanaDate % (msRealDay)) / (60 * 60 * 1000))
+    const vMin  = Math.floor((vanaDate % (60 * 60 * 1000)) / (60 * 1000))
+    const vSec  = Math.floor((vanaDate % (60 * 1000)) / 1000)
+    const vDay  = Math.floor((vanaDate % (8 * msRealDay)) / (msRealDay))
+
+    const vanaBaseSec = 92514960
+    const vanaTotalPassSec = (moment().unix() + vanaBaseSec) * 25
     const vanaMoon = Math.floor( (vanaTotalPassSec + 24 * 60 * 60 * 2) / (24 * 60 * 60 * 7) ) % 12
-    let vanaCalcSec = vanaTotalPassSec
-    const vanaYear = Math.floor( vanaCalcSec / (360 * 24 * 60 * 60) )
-    vanaCalcSec = vanaCalcSec % (360 * 24 * 60 * 60)
-    const vanaMonth = Math.floor( vanaCalcSec / (30 * 24 * 60 * 60) + 1 )
-    vanaCalcSec = vanaCalcSec % (30 * 24 * 60 * 60)
-    const vanaDay = Math.floor( vanaCalcSec /  (24 * 60 * 60) + 1 )
-    vanaCalcSec = vanaCalcSec % (24 * 60 * 60)
-    const vanaHour = Math.floor( vanaCalcSec / (60 * 60) )
-    vanaCalcSec = vanaCalcSec % (60 * 60)
-    const vanaMinute = Math.floor( vanaCalcSec / (60) )
-    vanaCalcSec = vanaCalcSec % (60)
-    const vanaSec = Math.floor(vanaCalcSec)
+
     return {
-      vanaWeek,
-      vanaWeekJp: weekDays[vanaWeek],
+      vDay,
+      vanaWeekJp: weekDays[vDay],
       vanaMoon,
       vanaMoonJp: moonList[vanaMoon],
-      vanaYear,
-      vanaMonth,
-      vanaDay,
-      vanaHour,
-      vanaMinute,
-      vanaSec
+      vanaYear: vYear,
+      vanaMonth: vMon,
+      vanaDay: vDate,
+      vanaHour: vHour,
+      vanaMinute: vMin,
+      vanaSec: vSec,
+      nextLightDay: moment(nextLightDay).utcOffset('+09:00')
     }
   },
   jsToImmutable: (obj) => {
--- test/utils/index.spec.js
+++ test/utils/index.spec.js
@@ -2,15 +2,50 @@ const moment = require('moment')
 
 const utils = require('../../src/utils')
 
-test('vanaTime', () => {
-  const unixMilliseconds = moment('2002/01/01 00:00:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
+test('2005/12/29 00:00:00のヴァナ時間', () => {
+  const unixMilliseconds = moment('2005/12/29 00:00:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
   Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
 
   const time = utils.vanaTime()
-  expect(time.vanaYear).toBe(886)
-  expect(time.vanaMonth).toBe(1)
+  expect(time.vanaYear).toBe(987)
+  expect(time.vanaMonth).toBe(4)
   expect(time.vanaDay).toBe(1)
   expect(time.vanaHour).toBe(0)
   expect(time.vanaMinute).toBe(0)
   expect(time.vanaSec).toBe(0)
+  expect(time.vanaWeekJp).toBe('水')
+  expect(time.vanaMoonJp).toBe('二十六夜')
+  expect(time.nextLightDay.format('YYYY/MM/DD HH:mm')).toBe('2005/12/29 03:50')
+})
+
+test('2005/12/25 00:00:01のヴァナ時間', () => {
+  const unixMilliseconds = moment('2005/12/25 00:00:01', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
+  Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
+
+  const time = utils.vanaTime()
+  expect(time.vanaYear).toBe(986)
+  expect(time.vanaMonth).toBe(12)
+  expect(time.vanaDay).toBe(21)
+  expect(time.vanaHour).toBe(0)
+  expect(time.vanaMinute).toBe(0)
+  expect(time.vanaSec).toBe(25)
+  expect(time.vanaWeekJp).toBe('光')
+  expect(time.vanaMoonJp).toBe('居待月')
+  expect(time.nextLightDay.format('YYYY/MM/DD HH:mm')).toBe('2005/12/25 07:40')
+})
+
+test('2005/12/26 00:00:00のヴァナ時間', () => {
+  const unixMilliseconds = moment('2005/12/26 00:00:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
+  Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
+
+  const time = utils.vanaTime()
+  expect(time.vanaYear).toBe(987)
+  expect(time.vanaMonth).toBe(1)
+  expect(time.vanaDay).toBe(16)
+  expect(time.vanaHour).toBe(0)
+  expect(time.vanaMinute).toBe(0)
+  expect(time.vanaSec).toBe(0)
+  expect(time.vanaWeekJp).toBe('闇')
+  expect(time.vanaMoonJp).toBe('新月')
+  expect(time.nextLightDay.format('YYYY/MM/DD HH:mm')).toBe('2005/12/26 06:43')
 })
