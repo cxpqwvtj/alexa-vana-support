a2c9d402d65b8a6d8a5947bbdd7bb0c87b73b218
cxpqwvtj <cxpqwvtj@gmail.com>
2018-01-02 10:21:01 +0900

    地球時間で何時間後かを回答する様に修正

--- src/handlers/nextLightDay.js
+++ src/handlers/nextLightDay.js
@@ -10,5 +10,9 @@ module.exports = function() {
     ? nextLightDay.format('HH時mm分')
     : nextLightDay.format('DD日HH時mm分')
   
-  this.emit(':tell', `次の 光 曜日は、地球時間の${message} です`)
+  const totalDiffMinutes = nextLightDay.diff(moment(), 'minutes')
+  const diffHours = totalDiffMinutes > 60 ? `${Math.floor(totalDiffMinutes / 60)}時間` : ''
+  const diffMinutes = totalDiffMinutes % 60 === 0 ? '' : `${totalDiffMinutes % 60}分`
+
+  this.emit(':tell', `次の 光 曜日は、${diffHours}${diffMinutes}後の、地球時間 ${message} です`)
 }
--- test/handlers/nextLightDay.spec.js
+++ test/handlers/nextLightDay.spec.js
@@ -7,7 +7,7 @@ test('2002/01/01 00:00:00の次の光曜日', () => {
   Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
 
   handlers.emit = (command, message) => {
-    expect(message).toBe('次の 光 曜日は、地球時間の05時45分 です')
+    expect(message).toBe('次の 光 曜日は、5時間45分後の、地球時間 05時45分 です')
   }
   handlers.nextLightDay()
 })
@@ -17,7 +17,7 @@ test('2003/06/24 00:00:00の次の光曜日', () => {
   Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
 
   handlers.emit = (command, message) => {
-    expect(message).toBe('次の 光 曜日は、地球時間の02時52分 です')
+    expect(message).toBe('次の 光 曜日は、2時間52分後の、地球時間 02時52分 です')
   }
   handlers.nextLightDay()
 })
@@ -27,17 +27,17 @@ test('2003/06/23 23:00:00の次の光曜日', () => {
   Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
 
   handlers.emit = (command, message) => {
-    expect(message).toBe('次の 光 曜日は、地球時間の24日02時52分 です')
+    expect(message).toBe('次の 光 曜日は、3時間52分後の、地球時間 24日02時52分 です')
   }
   handlers.nextLightDay()
 })
 
-test('2018/01/02 09:16:00の次の光曜日', () => {
-  const unixMilliseconds = moment('2018/01/02 09:16:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
+test('2018/01/02 09:19:00の次の光曜日', () => {
+  const unixMilliseconds = moment('2018/01/02 09:19:00', 'YYYY/MM/DD HH:mm:ss').utcOffset('+09:00').valueOf()
   Date.now = jest.fn(() => new Date(unixMilliseconds)) // 現在時刻のmock
 
   handlers.emit = (command, message) => {
-    expect(message).toBe('次の 光 曜日は、地球時間の16時19分 です')
+    expect(message).toBe('次の 光 曜日は、7時間後の、地球時間 16時19分 です')
   }
   handlers.nextLightDay()
 })
